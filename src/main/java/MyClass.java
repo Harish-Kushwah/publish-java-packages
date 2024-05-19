5d3476d24fa74571c4995b02c06e82ee55e27bd2
Harish-Kushwah <harishkushwah54321@gmail.com>
2024-05-19 18:10:28 +0530

    Updated

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -7,4 +7,9 @@ public class MyClass {
     {
         System.out.println(msg);
     }
+    public static void main(String args[])
+    {
+        MyClass m = new MyClass();
+        m.printText("hello");
+    }
 }
